f29cb9b3f6cc97aa33df88ad8ba9d2b695c3c0c0
Ryan Cutter <ryancutter.mail@gmail.com>
2019-03-18 07:33:28 +0000

    4 new Phils!

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const philers = [
   //Please check list to avoid duplications
   //and please keep in alphabetical order
   "Big Phily Style",
+  "Bitter Phil to swallow",
   "Don't be afraid to catch Phils",
   "Girls on Philm",
   "Ian Phil",
+  "Jagged little Phil",
   "Last Phil and testament",
   "My heart Phil go on",
   "One Phil on my wagon",
@@ -15,6 +17,7 @@ const philers = [
   "Phil switch engage",
   "Phil your boots",
   "Philbo Baggins",
+  "Philed of Dreams",
   "Philiam the conqueror",
   "Philing for divorce",
   "Philer of the community",
@@ -24,7 +27,8 @@ const philers = [
   "Tears on my Philow",
   "The Phils are alive with the sound of music",
   "Three Philed Robin Reliant",
-  "Where there's a Phil, there's a way"
+  "Where there's a Phil, there's a way",
+  "Windy Philler"
 ];
 
 const philer = () => philers[Math.floor(Math.random() * philers.length)];
